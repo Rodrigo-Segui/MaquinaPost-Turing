3cf929630c0211e5a3a18a186cfce574d7539901
filhodeDeus <you@example.com>
2019-10-10 02:49:16 -0300

    leitura entrada

---
+++ codigos/Main.py
@@ -0,0 +1,17 @@
+##autores Rodrigo Segui, Marcelo Marchioro, Arthur Teixeira, Gabriel Velasco
+import sys
+from classes.Leitor import Leitor
+def main():
+    #try:
+        if(len(sys.argv) != 3):
+            print("Modo de execucao: ./Main.py <arquivo-de-entrada> <arquivo-de-saida>")
+            print("Obs: O arquivo de entrada deve estar no diretorio \"arquivos/estradas\" "
+                   + "deste projeto")
+            return
+
+        entrada = "../arquivos/entradas/" + sys.argv[1]
+        saida = "../arquivos/saidas/" + sys.argv[2]
+
+        leituraMP = Leitor(entrada)
+
+main()
---
+++ codigos/classes/Leitor.py
@@ -0,0 +1,17 @@
+import re
+
+class Leitor:
+    arquivo = None
+    linhas = None
+    estadoInicial = None
+    estadosFinais = None
+    estados = None
+    transicoes = None
+    alfabeto = None
+    todosSimbolos = None
+    
+    def __init__(self, arquivo):
+        self.arquivo = open(arquivo, "r")
+        print(self.arquivo.read())
+       # self.leDados()
+        self.arquivo.close()
